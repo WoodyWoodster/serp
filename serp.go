3c8d8f14f677dbdd4d1fed4c1eefdf04491008b1
Andrew Woodward <11779108+WoodyWoodster@users.noreply.github.com>
2025-06-10 21:16:45 -0500

    created serp

--- serp.go
+++ serp.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"os"
+	"serp/infrastructure/services"
+	"serp/infrastructure/shared"
+
 	"github.com/aws/aws-cdk-go/awscdk/v2"
-	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
+
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
 )
@@ -18,13 +22,6 @@ func NewSerpStack(scope constructs.Construct, id string, props *SerpStackProps) 
 	}
 	stack := awscdk.NewStack(scope, &id, &sprops)
 
-	// The code that defines your stack goes here
-
-	// example resource
-	// queue := awssqs.NewQueue(stack, jsii.String("SerpQueue"), &awssqs.QueueProps{
-	// 	VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(300)),
-	// })
-
 	return stack
 }
 
@@ -33,38 +30,43 @@ func main() {
 
 	app := awscdk.NewApp(nil)
 
-	NewSerpStack(app, "SerpStack", &SerpStackProps{
-		awscdk.StackProps{
+	// Create shared infrastructure stack
+	shared.NewSharedStack(app, "ErpSharedStack", &shared.SharedStackProps{
+		StackProps: awscdk.StackProps{
 			Env: env(),
 		},
+	})
+
+	// Create Inventory microservice stack
+	services.NewMicroserviceStack(app, "InventoryServiceStack", &services.MicroserviceStackProps{
+		StackProps: awscdk.StackProps{
+			Env: env(),
+		},
+		ServiceName:     "inventory",
+		VpcId:           awscdk.Fn_ImportValue(jsii.String("ErpVpcId")),
+		SecurityGroupId: awscdk.Fn_ImportValue(jsii.String("ErpLambdaSecurityGroupId")),
+		LambdaRoleArn:   awscdk.Fn_ImportValue(jsii.String("ErpLambdaRoleArn")),
+		GraphqlApiId:    awscdk.Fn_ImportValue(jsii.String("ErpGraphqlApiId")),
+	})
+
+	// Create Orders microservice stack
+	services.NewMicroserviceStack(app, "OrdersServiceStack", &services.MicroserviceStackProps{
+		StackProps: awscdk.StackProps{
+			Env: env(),
+		},
+		ServiceName:     "orders",
+		VpcId:           awscdk.Fn_ImportValue(jsii.String("ErpVpcId")),
+		SecurityGroupId: awscdk.Fn_ImportValue(jsii.String("ErpLambdaSecurityGroupId")),
+		LambdaRoleArn:   awscdk.Fn_ImportValue(jsii.String("ErpLambdaRoleArn")),
+		GraphqlApiId:    awscdk.Fn_ImportValue(jsii.String("ErpGraphqlApiId")),
 	})
 
 	app.Synth(nil)
 }
 
-// env determines the AWS environment (account+region) in which our stack is to
-// be deployed. For more information see: https://docs.aws.amazon.com/cdk/latest/guide/environments.html
 func env() *awscdk.Environment {
-	// If unspecified, this stack will be "environment-agnostic".
-	// Account/Region-dependent features and context lookups will not work, but a
-	// single synthesized template can be deployed anywhere.
-	//---------------------------------------------------------------------------
-	return nil
-
-	// Uncomment if you know exactly what account and region you want to deploy
-	// the stack to. This is the recommendation for production stacks.
-	//---------------------------------------------------------------------------
-	// return &awscdk.Environment{
-	//  Account: jsii.String("123456789012"),
-	//  Region:  jsii.String("us-east-1"),
-	// }
-
-	// Uncomment to specialize this stack for the AWS Account and Region that are
-	// implied by the current CLI configuration. This is recommended for dev
-	// stacks.
-	//---------------------------------------------------------------------------
-	// return &awscdk.Environment{
-	//  Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
-	//  Region:  jsii.String(os.Getenv("CDK_DEFAULT_REGION")),
-	// }
+	return &awscdk.Environment{
+		Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
+		Region:  jsii.String(os.Getenv("CDK_DEFAULT_REGION")),
+	}
 }
